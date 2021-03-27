9dc0903898724ce20c6a1f32558e63457d34fd0e
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2021-03-27 04:53:53 -0300

    Problema 1 - 1a Implementacao

--- Sistemas de Comunicacoes Digitais/matlab/script.m
+++ Sistemas de Comunicacoes Digitais/matlab/script.m
@@ -1,5 +1,5 @@
 %% Trabalho de SCD 
-% Problema 1 - Modulacao M-QAM 
+% Testes
 % 2021/03/24 - Lucas Abdalah
 %
 
@@ -13,20 +13,26 @@ L = 4096;       % - Tamanho da sequencia (bits)
 K = log2(M);    % - Numero de bits/simbolo
 N = L/K;        % - Numero de simbolos a serem transmitidos
 
-s_m = randi([0 1],L,1)';   % - Mensagem a transmitir
+% s_m = randi([0 1],L,1)';   % - Mensagem a transmitir
 
 % Generating a gray const.
-G = gray_const(M,true);
+% G = gray_const(M,true);
 
-% vec2flip = 1:M;
-% Hi, I have a matrix xx which is a 6x20 matrix, and I want to flip only the even rows. Im trying this but is doesn't work:
+x = 1 + 1j*1;
+y = -1 + 0j*1;
 
-% xx = reshape([1:12],[3,4])
-% xx_flip = xx;
-% xx_flip(2:2:end,:) = fliplr(xx(2:2:end,:))
+d_E = sqrt( (real(x)-real(y))^2 + (imag(x)-imag(y))^2)
 
-% Funcao para flipar linha pares da matriz e mapear no codigo de Gray
-% prepare_const2gray(const)
 
-sqrt(M) % Cada linha apresenta sqrt(M) simbolos, entao para ordenar de modo a ter a codificao de Gray, teremos que inverter as linhas pares da constelacao
 
+d = distancia_QAM(1);
+M = 4
+a = randn(1);
+r = a + 1j*a
+[symb,bits]=demapping_MQAM(r,M(1),d)
+disp('---------------------------------------')
+symb = mapping_MQAM(bits)
+figure(hfig(1));
+scatter(real(r),imag(r));
+hold on 
+hold off
