13084492a1b3525a377d9226969aaaebf0885f8c
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2021-03-30 20:48:58 -0300

    Update script.m

--- Sistemas de Comunicacoes Digitais/matlab/script.m
+++ Sistemas de Comunicacoes Digitais/matlab/script.m
@@ -1,5 +1,4 @@
-%% Trabalho de SCD 
-% Testes
+%% Trabalho de SCD
 % 2021/03/24 - Lucas Abdalah
 %
 
@@ -7,17 +6,51 @@ close all; clearvars; clc; % Clear the matlab ambient
 
 % addpath 'C:\Users\lukin\Documents\GitHub\Courses-HWs\Sistemas de Comunicacoes Digitais\matlab\problema1'
 addpath 'problema1' % Local path
+addpath 'problema2' % Local path
+addpath 'problema3' % Local path
 addpath 'problema4' % Local path
+addpath 'problema5' % Local path
+
+script_MQAM;
+disp('>> pressione qualquer botao para continuar para questao 2');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
+
+script_prob;
+disp('>> pressione qualquer botao para continuar para questao 3 - parte 1');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
+
+script_AWGN;
+disp('>> pressione qualquer botao para continuar para questao 3 - parte 2');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
+
+script_teoricaxAWGN;
+disp('>> pressione qualquer botao para continuar para questao 4 - parte 1');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
+
 addpath 'problema4\parte1' % Local path
+addpath 'problema4\parte2' % Local path
+addpath 'problema4\parte3' % Local path
+rmpath('problema1') % Local path
+rmpath('problema2') % Local path
+rmpath('problema3') % Local path
 
-M = 4;         % - Numero de simbolos da constelacao
-% L = 4096;       % - Tamanho da sequencia (bits)
-K = log2(M);    % - Numero de bits/simbolo
-% N = L/K;        % - Numero de simbolos a serem transmitidos
+script_MPSK;
+disp('>> pressione qualquer botao para continuar para questao 4 - parte 2');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
 
-% s_m = randi([0 1],L,1)';   % - Mensagem a transmitir
+script_prob;
+disp('>> pressione qualquer botao para continuar para questao 4 - parte 3');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
 
-% Generating a gray const.
-% G = gray_const(M,true);
+script_teoricaxAWGN;
+disp('>> pressione qualquer botao para continuar para questao 5');
+pause;
+close all; clearvars; clc; % Clear the matlab ambient
 
-%% Problema 5 - Calculo da energia media (E_m) e da distancia entre os simbolos
+
