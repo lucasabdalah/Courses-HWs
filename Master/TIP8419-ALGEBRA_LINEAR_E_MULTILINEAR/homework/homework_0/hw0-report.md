732fe3da66a1fd487fd8a3ca1b242346c39ae97b
lucasabdalah <lucasabdalah@gmail.com>
2022-05-17 22:07:42 -0300

    task: Problem1_b-Method1

---
+++ Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/homework_0/hw0-report.md
@@ -0,0 +1,106 @@
+# TI8419 - Multilinear Algebra
+# Lucas Abdalah
+## Homework 0
+
+See the section on [`Problem 1`](#Method1)
+
+## Table of Contents
+1. [Problem 1](#problem-1)
+2. [Problem 2](#problem-2)
+
+
+## Problem 1
+### Item (a) 
+
+> Method 1
+  
+$$ (\mathbf{A}_{N \times N} \otimes \mathbf{B}_{N \times N} )^{-1}$$
+
+- Method 2
+
+$$ (\mathbf{A}_{N \times N})^{-1} \otimes (\mathbf{B}_{N \times N})^{-1}$$
+
+for $n \in \{2,4,6,8,16,32,64\}$
+
+- - -
+
+## Problem 2
+
+### (a) Method 1
+$$ (\mathbf{A}_{N \times N} \otimes \mathbf{B}_{N \times N} )^{-1}$$
+
+### (b) Method 2
+
+The script to peform Problem B with matrices NxN and kron productory until K, with N = 4, and K = 8, results in an dimensional error, since it's required more the RAM memory available.
+
+```
+Requested 4x16384x4x16384 (64.0GB) array exceeds maximum array size preference (19.8GB). This might cause MATLAB to become unresponsive.
+
+Error in kron (line 35)
+   K = reshape(A.*B, ma*mb, na*nb);
+
+Error in problem1_itemb>b_method1 (line 43)
+            C = kron(C, A{ii});
+
+Error in problem1_itemb (line 16)
+[time, ~] = b_method1(8)
+```
+<div style="background-color:rgba(250, 0, 0, 0.25); text-align:left; padding:20px">
+<p> 
+
+``` 
+Requested 4x16384x4x16384 (64.0GB) array exceeds maximum array size preference (19.8GB). This might cause MATLAB to become unresponsive. 
+
+Error in kron (line 35)
+   K = reshape(A.*B, ma*mb, na*nb);
+
+Error in problem1_itemb>b_method1 (line 43)
+            C = kron(C, A{ii});
+
+Error in problem1_itemb (line 16)
+[time, ~] = b_method1(8)
+``` 
+</p>
+</div>
+
+
+
+with N = 4, and K = 8
+
+
+```
+problem1_itemb
+       16384       16384
+
+time =
+
+  486.4500
+```
+
+- - - 
+
+[Hobbit lifestyles][1]
+
+
+<!-- References -->
+
+[1]: <https://en.wikipedia.org/wiki/Hobbit#Lifestyle> (Hobbit lifestyles)
+
+
+<!-- <span style="color:red">some **blue** text</span>. -->
+
+<!-- <div style="background-color:rgba(250, 0, 0, 0.25); text-align:center; padding:20px">
+<p> Alo teste </p>
+</div>
+
+<div style="background-color:rgba(0, 0, 250, 0.25); text-align:center; padding:20px">
+<p> Alo teste </p>
+</div>
+
+<div style="background-color:rgba(0, 250, 0, 0.25); text-align:center; padding:20px">
+<p> Alo teste </p>
+</div> -->
+
+<!-- <div style="background-color:rgba(0, 0, 0, 0.0470588); text-align:center; vertical-align: middle; padding:40px 0; margin-top:30px">
+<a href="/blog">VIEW THE BLOG</a>
+</div> -->
--- Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/homework_0/problem_1.m
+++ Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/homework_0/problem_1.m
@@ -6,66 +6,82 @@ function output = problem_1
     % Long description
     
     % Item A
-    data.MC = 2;
-    data.N = 2.^(1:6);
-    data.time_a_method1 = zeros(data.MC, length(data.N));
-    data.time_a_method2 = zeros(data.MC, length(data.N));
+    % data.MC = 2;
+    % data.N = 2.^(1:6);
+    % data.time_a_method1 = zeros(data.MC, length(data.N));
+    % data.time_a_method2 = zeros(data.MC, length(data.N));
 
-    for ii = 1:1:length(data.N)
-        data.A{ii} = complex(randn(data.N(ii), data.N(ii)), randn(data.N(ii),data.N(ii)));
-        data.B{ii} = complex(randn(data.N(ii), data.N(ii)), randn(data.N(ii),data.N(ii)));
-    end
+    % for ii = 1:1:length(data.N)
+    %     data.A{ii} = complex(randn(data.N(ii), data.N(ii)), randn(data.N(ii),data.N(ii)));
+    %     data.B{ii} = complex(randn(data.N(ii), data.N(ii)), randn(data.N(ii),data.N(ii)));
+    % end
     
-    for RMC = 1:1:data.MC
+    % for RMC = 1:1:data.MC
         
-        fprintf('Monte Carlo Step: %2f ...\n', RMC)
+    %     fprintf('Monte Carlo Step: %2f ...\n', RMC)
 
-        for ii = 1:1:length(data.N)
+    %     for ii = 1:1:length(data.N)
             
-            fprintf('Matrix Dimension Step: %2f ... \n', ii)
+    %         fprintf('Matrix Dimension Step: %2f ... \n', ii)
 
-            [data.time_a_method1(RMC,ii)] = a_method1(data.A{ii},data.B{ii});
-            [data.time_a_method2(RMC,ii)] = a_method2(data.A{ii},data.B{ii});
-        end
-    end
+    %         [data.time_a_method1(RMC,ii)] = a_method1(data.A{ii},data.B{ii});
+    %         [data.time_a_method2(RMC,ii)] = a_method2(data.A{ii},data.B{ii});
+    %     end
+    % end    
     
-    
-    output = data;
+    % output = data;
 
     % Item B
 
+    % data2.MC = 2;
+    % data2.N = 2.^(1:6);
+    % data2.time_a_method1 = zeros(data.MC, length(data.N));
+    % data2.time_a_method2 = zeros(data.MC, length(data.N));
+    K = 2:2:10;
+    N = length(K);
+    time_cost = zeros(MC, N);
+
+    for ii = 1:1:N
+        for jj = 1:1:K(ii)
+            time_cost(ii) = b_method1(K);
+            disp(jj)
+        end
+    end
+
+    output = time_cost;
     
 end
 
 % Item A
-function [time] = a_method1(A, B)
-    tic;
-    inv(kron(A,B));
-    time = toc;
-end
-
-function [time] = a_method2(A, B)
-    tic;
-    kron(inv(A),inv(B));
-    time = toc;
-end
-
-% % Item B
-% function [time, C] = b_method1(K)
-%     N = 4;
-    
-%     % Generate matrices
-%     for ii = 1:K
-%         A{ii} = complex(randn(N, N), randn(N,N));
-%     end
-    
+% function [time] = a_method1(A, B)
 %     tic;
-%     % Produtorio
-%     for ii = 1:K
-        
-%     end
+%     inv(kron(A,B));
 %     time = toc;
 % end
+
+% function [time] = a_method2(A, B)
+%     tic;
+%     kron(inv(A),inv(B));
+%     time = toc;
+% end
+
+% % Item B
+function [time] = b_method1(K)
+    N = 4;
+    C = ones(4,4);
+    A = cell(K,1);
+    % Generate matrices
+    for ii = 1:K
+        A{ii} = complex(randn(N, N), randn(N,N));
+    end
+    
+    tic;
+    % Produtorio
+    for ii = 1:1:K
+        C = C*A{ii};
+    end
+    time = toc;
+end
 
 % function [time, C] = b_method2(N)
 %     tic;
@@ -73,9 +89,6 @@ end
 %     time = toc;
 % end
 
-
-
-
-% function rnd_COMPLEX_MATRIX = gen_COMPLEX_MATRIX(N)
-%     rnd_COMPLEX_MATRIX = complex(randn(N, N), randn(N,N));
+% function complex_matrix = randn_complex_matrix(N)
+%     complex_matrix = complex(randn(N, N), randn(N,N));
 % end
