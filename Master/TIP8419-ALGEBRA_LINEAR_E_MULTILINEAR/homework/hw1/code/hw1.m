9bdb3e2943e6931ca0f4a068144b4718da1a5304
lucasabdalah <lucasabdalah@gmail.com>
2022-06-25 18:02:20 -0300

    finish results hw1

--- Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/hw1/code/hw1.m
+++ Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/hw1/code/hw1.m
@@ -9,11 +9,12 @@
 % clc;
 clearvars; close all; 
 
-% %% hw1_problem1
+
+%% hw1_problem1
 problem1 = load('hw1_problem1_data.mat');
 
 % Plot results
-h1 = figure();
+h_problem1 = figure();
 loglog(problem1.N, mean(problem1.time_method_mine, 1),...
     'Color', 'blue',...        
     'LineStyle', '--',...
@@ -40,14 +41,14 @@ legend boxoff
 grid on
 axis tight
 
-% savefig_tight(h1, "figures/hw1-problem1", "both")
+% savefig_tight(h_problem1, "figures/hw1-problem1", "both")
 
 
 %% hw1_problem2
 problem2 = load('hw1_problem2_data.mat');
 
 % Plot results
-h2 = figure();
+h_problem2 = figure();
 loglog(problem2.N, mean(problem2.time_method_author, 1),...
     'Color', 'blue',...        
     'LineStyle', '--',...
@@ -74,4 +75,27 @@ legend boxoff
 grid on
 axis tight
 
-% savefig_tight(h2, "figures/hw1-problem2", "both")
+% savefig_tight(h_problem2, "figures/hw1-problem2", "both")
+
+
+%% hw1_problem3
+problem3 = load('hw1_problem3_data.mat');
+
+% Plot results
+h_problem3 = figure();
+loglog(problem3.N, mean(problem3.time_method_author, 1),...
+    'Color', 'blue',...        
+    'LineStyle', '--',...
+    'LineWidth', 1.0,...
+    'Marker', 'o',...
+    'MarkerFaceColor', 'blue',...
+    'MarkerSize', 5);
+xticks(problem3.N);
+xlabel('Matrix Dimension, N')
+ylabel('Time (s)')
+legend(["Author"], 'Location', 'Best') % legend(leg, 'Location', 'Northeastoutside')
+legend boxoff
+grid on
+axis tight
+
+savefig_tight(h_problem3, "figures/hw1-problem3", "both")
