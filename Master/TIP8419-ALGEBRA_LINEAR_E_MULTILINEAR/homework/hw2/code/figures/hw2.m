aa05ececff41658337c01ab618a25be11f5d0e37
lucasabdalah <lucasabdalah@gmail.com>
2022-06-27 01:35:41 -0300

    organize hw2-report

---
+++ Master/TIP8419-ALGEBRA_LINEAR_E_MULTILINEAR/homework/hw2/code/figures/hw2.m
@@ -0,0 +1,10 @@
+%% [TIP8419 - Algebra Linear e Multilinear] - Homework 1
+% by Lucas Abdalah
+% ----------------------------
+% Problem
+% hw2.m
+% Author: Lucas Abdalah
+%
+
+% clc;
+clearvars; close all; 
